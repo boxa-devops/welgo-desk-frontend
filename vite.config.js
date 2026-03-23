7ff139544a21fe9c6239732035980e44505ef9a0
boxa-devops <boxa.devops@gmail.com>
2026-03-23 19:37:30 +0500

    upd

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => {
         '/api': {
           target: env.WELGO_DESK_BACKEND_URL || 'http://localhost:8000',
           changeOrigin: true,
+          secure: false,
         },
       },
     },
