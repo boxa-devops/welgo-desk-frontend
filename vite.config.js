2680bcc2646578bb4622d7ec41f7da2ca81bd85c
boxa-devops <boxa.devops@gmail.com>
2026-03-23 19:23:51 +0500

    u[pd

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import preact from '@preact/preset-vite';
 
-export default defineConfig({
-  plugins: [preact()],
-  build: {
-    outDir: '../welgo-desk/static',
-    emptyOutDir: true,
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8888',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [preact()],
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.WELGO_DESK_BACKEND_URL || 'http://localhost:8000',
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
